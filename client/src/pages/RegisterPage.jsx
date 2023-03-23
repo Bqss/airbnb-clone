0c15eecdd77042d7873abd00eafc5d3999a8c2ac
aufall02 <aufaluzumaki@gmail.com>
2023-03-23 11:25:01 +0700

    gass

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function RegisterPage() {
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register Page</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="example@gmail.com" />
+          <input type="password" placeholder="password" />
+          <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            Don't have an account yet? <Link className="underline text-black" to={"/register"}>Register Now</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
