1399144aebde42daa4b6d9501ef42bf17a033063
aufall02 <aufaluzumaki@gmail.com>
2023-03-25 13:51:31 +0700

    setup database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+const { json } = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+//
 
 app.get("/test", (req, res) => {
   res.json("test oke");
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  res.json({ name, email, password });
 });
 
 app.listen(4000);
