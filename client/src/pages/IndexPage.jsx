a982668469f0b0c702a9afe43f85f8d4f0a08784
Bqss <basofi.cucokmeong12@gmail.com>
2023-04-02 17:27:07 +0700

    finishing

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -2,15 +2,26 @@ import { useState } from "react";
 import Modal from "../components/Modal";
 import RegisterModal from "../components/modals/RegisterModal";
 import { useDispatch, useSelector } from "react-redux";
-import { toggleOpenRegisterModal } from "../fitures/modalSlice";
+import { toggleOpenLoginModal, toggleOpenRegisterModal } from "../fitures/modalSlice";
+import LoginModal from "../components/modals/LoginModal";
 
 export default function IndexPage() {
-  const {openLoginModal, openRegisterModal} = useSelector(state => state.modal);
+  const { openLoginModal, openRegisterModal } = useSelector(
+    (state) => state.modal
+  );
   const dispatch = useDispatch();
 
   return (
-    <div className="" >
-      <RegisterModal isOpen={openRegisterModal} onClose={() => dispatch(toggleOpenRegisterModal())}/>
-    </div>
+    <>
+      <RegisterModal
+        isOpen={openRegisterModal}
+        onClose={() => dispatch(toggleOpenRegisterModal())}
+      />
+      <LoginModal
+        isOpen={openLoginModal}
+        onClose={() => dispatch(toggleOpenLoginModal())}
+      />
+      <div className=""></div>
+    </>
   );
 }
