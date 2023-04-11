ec43f235662d76206bac206d47b579eeb4b2a451
Bqss <basofi.cucokmeong12@gmail.com>
2023-04-11 08:30:52 +0700

    custom radio button

--- client/src/components/atoms/RadioButton.jsx
+++ client/src/components/atoms/RadioButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RadioButton = ({ children, className, name, value, setSelected, selected }) => {
   return (
     <label className="block flex-1 cursor-pointer">
-      <input type="radio" value={value} checked={selected} onChange={() => setSelected(value)} name={name} className="peer hidden check" />
+      <input type="radio" value={value} checked={selected} onChange={() => setSelected(value)} name={name} className="peer hidden" />
       <div className={["border-2 transition-colors duration-150 p-5 border-gray-200  rounded-lg  hover:border-black peer-checked:border-black peer-checked:[&>svg]:animate-pop ",className].join(" ")}>
         {children}
       </div>
