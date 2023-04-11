bdba76f9bd98f18003fde9456089cb7f833ee577
Bqss <basofi.cucokmeong12@gmail.com>
2023-04-11 08:31:10 +0700

    fixed data

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,94 +1,188 @@
-import {FaSwimmingPool} from "react-icons/fa"
-import { GiWindmill, GiModernCity, GiVillage, GiAntarctica, GiIsland } from "react-icons/gi";
-import { TbBeach } from "react-icons/tb";
+import {FaFireExtinguisher, FaSwimmingPool} from "react-icons/fa"
+import { GiWindmill, GiModernCity, GiVillage, GiAntarctica, GiIsland, GiFireplace, GiGrandPiano } from "react-icons/gi";
+import { TbAirConditioning, TbBeach, TbPool } from "react-icons/tb";
 import {HiOutlineHomeModern} from "react-icons/hi2"
 import {CgTrees} from "react-icons/cg"
 import {GrRestroomMen} from "react-icons/gr"
-import {IoHomeOutline} from "react-icons/io5"
+import {IoBarbellOutline, IoHomeOutline} from "react-icons/io5"
 import {TfiGame, TfiHome} from "react-icons/tfi"
-import {MdHotel} from "react-icons/md";
-import {BiAccessibility} from "react-icons/bi"
+import {MdHotel, MdSoupKitchen} from "react-icons/md";
+import {BiAccessibility, BiBath} from "react-icons/bi"
+import { AiOutlineCar, AiOutlineDesktop, AiOutlineMedicineBox, AiOutlineWifi } from "react-icons/ai";
+import {BsTv} from "react-icons/bs"
 
 
 
 
 const desc = [
     {
+        id: "1",
         label : "Dekat Pantai",
         value : "dekatpantai",
         icon : TbBeach
     },
     {
+        id: "2",
         label : "Modern",
         value : "modern",
         icon : GiModernCity
     },
     {
+        id: "3",
         label : "Kincir Angin",
         value : "kincirangin",
         icon : GiWindmill
     },
     {
+        id: "4",
         label : "Pedesaan",
         value : "pedesaan",
         icon : GiVillage
     },
     {
+        id: "5",
         label : "Kolam Renang",
         value : "kolamrenang",
         icon : FaSwimmingPool
     },
     {
+        id: "6",
         label : "Desain Keren",
         value : "desainkeren",
         icon : HiOutlineHomeModern
     },
     {
+        id: "7",
         label : "Pemandangan Cantik",
         value : "pemandangancantik",
         icon : CgTrees
     },
     {
+        id: "8",
         label : "Kamar Pribadi",
         value : "kamarpribadi",
         icon : GrRestroomMen
     },
     {
+        id: "9",
         label : "Rumah Kecil",
         value : "rumahkecil",
         icon : IoHomeOutline
     },
     {
+        id: "10",
         label : "Terpencil",
         value : "terpencil",
         icon : TfiHome
     },
     {
+        id: "11",
         label : "Bermain",
         value : "bermain",
         icon : TfiGame
     },
     {
+        id: "12",
         label : "Arktika",
         value : "arktika",
         icon : GiAntarctica
     },
     {
+        id: "13",
         label : "Pulau",
         value : "pulau",
         icon : GiIsland
     },
     {
+        id: "13",
         label : "Mansion",
         value : "mansion",
         icon : MdHotel
     },
     {
+        id: "14",
         label : "Aksebilitas",
         value : "arktika",
         icon : BiAccessibility
     }
-    
+]
+
+
+
+
+export const fasility = [
+    {
+        id: "1",
+        label : "Wifi",
+        value : "wifi",
+        icon : AiOutlineWifi
+    },
+    {
+        id: "2",
+        label : "Televisi",
+        value : "televisi",
+        icon : AiOutlineDesktop
+    },
+    {
+        id: "3",
+        label : "Dapur",
+        value : "dapur",
+        icon : MdSoupKitchen
+    },
+    {
+        id: "4",
+        label : "Tempat parkir",
+        value : "tempatparkir",
+        icon : AiOutlineCar
+    },
+    {
+        id: "5",
+        label : "AC",
+        value : "ac",
+        icon : TbAirConditioning
+    },
+    {
+        id: "6",
+        label : "Kolam renang",
+        value : "kolamrenang",
+        icon : TbPool
+    },
+    {
+        id: "7",
+        label : "Bak mandi",
+        value : "bakmandi",
+        icon : BiBath
+    },
+    {
+        id: "8",
+        label : "Perapian",
+        value : "perapian",
+        icon : GiFireplace
+    },
+    {
+        id: "9",
+        label : "Piano",
+        value : "piano",
+        icon : GiGrandPiano
+    },
+    {
+        id: "10",
+        label : "Peralatan olahraga",
+        value : "peralatanolahraga",
+        icon : IoBarbellOutline
+    },
+    {
+        id: "11",
+        label : "Peralatan P3K",
+        value : "p3k",
+        icon : AiOutlineMedicineBox
+    },
+    {
+        id: "12",
+        label : "Pemadam api",
+        value : "pemadamapi",
+        icon : FaFireExtinguisher
+    }
 ]
 
 
