2d2ea84a448ba88e422f69be5f369e08c4b0a774
Bqss <basofi.cucokmeong12@gmail.com>
2023-04-11 08:31:19 +0700

    state management

--- client/src/components/modals/CreateNewBnbModal.jsx
+++ client/src/components/modals/CreateNewBnbModal.jsx
@@ -6,24 +6,31 @@ import Tabs from "../Tabs";
 import Button from "../atoms/Button";
 
 import RadioButton from "../atoms/RadioButton";
-import { desc } from "../../data";
+import { desc, fasility } from "../../data";
 import { GrRestroomMen } from "react-icons/gr";
 import { IoPeopleSharp } from "react-icons/io5";
 import Stepper from "../atoms/Stepper";
+import CheckBox from "../atoms/CheckBox";
+import FileInput from "../atoms/FileInput";
+import TextArea from "../atoms/TextArea";
 
 const STEPS = {
   CATEGORY: 0,
-  LOCATION: 1,
-  INFO: 2,
-  IMAGES: 3,
-  DESCRIPTION: 4,
-  PRICE: 5,
+  Tipe: 1,
+  LOCATION: 2,
+  INFO: 3,
+  FASILITAS: 4,
+  IMAGES: 5,
+  JUDUL: 6,
+  DESCRIPTION: 7,
+  PRICE: 8,
 };
 
 const CreateNewBnbModal = ({ isOpen, onClose }) => {
-  const [currentStep, setCurrentStep] = useState(STEPS.CATEGORY);
+  const [currentStep, setCurrentStep] = useState(STEPS.JUDUL);
   const [category, setCategory] = useState("");
   const [tipeRumah, setTipeRumah] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const [lokasi, setLokasi] = useState("");
   const [infoDasar, setInfoDasar] = useState({
     tamu: 1,
@@ -32,11 +39,16 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
     kamarMandi: 1,
   });
 
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
 
+  const next = () => {
+    setCurrentStep((e) => e + 1);
+  };
 
-
-
-
+  const prev = () => {
+    setCurrentStep((e) => e - 1);
+  };
 
   return (
     <Modal
@@ -65,9 +77,9 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
         <div className="flex-1 flex flex-col overflow-y-auto">
           <Tabs activeTab={currentStep} className="h-full ">
             <Tabs.Panels className="h-full">
-              <Tabs.Panel value={0}>
+              <Tabs.Panel value={STEPS.CATEGORY}>
                 <div className="w-full max-w-2xl mx-auto flex flex-col justify-center">
-                  <h2 className="text-2xl font-medium mt-5">
+                  <h2 className="text-3xl font-medium mt-5">
                     Opsi mana yang terbaik untuk mendeskripsikan tempat anda?{" "}
                   </h2>
                   <span className="text-gray-400 block mt-2">
@@ -90,18 +102,20 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
                   </div>
                 </div>
               </Tabs.Panel>
-              <Tabs.Panel value={1}>
+              <Tabs.Panel value={STEPS.Tipe}>
                 <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
-                  <h2 className="text-2xl font-medium mt-5">
+                  <h2 className="text-3xl font-medium mt-5">
                     Apa tipe tempat yang bisa digunakan tamu ?
                   </h2>
-                  <span className="text-gray-400 block mt-2">Pilih tipe</span>
+                  <span className="text-gray-400 block mt-2">
+                    Pilih tipe ruangan yang bisa digunakan oleh tamu
+                  </span>
                   <div className="flex flex-col gap-3 mt-8 ">
                     <RadioButton
                       value={"seluruhrumah"}
                       className="p-5 flex rounded-xl justify-between items-center"
                       name="tipe"
-                      selected={tipeRumah == 'seluruhrumah'}
+                      selected={tipeRumah == "seluruhrumah"}
                       setSelected={setTipeRumah}
                     >
                       <div className="space-y-1">
@@ -116,13 +130,14 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
                       value={"kamarpribadi"}
                       className="p-6 flex justify-between items-center"
                       name="tipe"
-                      selected={tipeRumah == 'kamarpribadi'}
+                      selected={tipeRumah == "kamarpribadi"}
                       setSelected={setTipeRumah}
                     >
                       <div className="space-y-1">
                         <h3 className="font-medium text-xl">Kamar pribadi</h3>
                         <p className="text-gray-400">
-                        Tamu akan tidur di kamar pribadi, namun sebagian area mungkin akan digunakan anda atau orang lain
+                          Tamu akan tidur di kamar pribadi, namun sebagian area
+                          mungkin akan digunakan anda atau orang lain
                         </p>
                       </div>
                       <GrRestroomMen className="w-10 h-10 flex-shrink-0" />
@@ -131,13 +146,15 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
                       value={"kamarbersama"}
                       className="p-6 flex justify-between items-center"
                       name="tipe"
-                      selected={tipeRumah == 'kamarbersama'}
+                      selected={tipeRumah == "kamarbersama"}
                       setSelected={setTipeRumah}
                     >
                       <div className="space-y-1">
                         <h3 className="font-medium text-xl">Kamar bersama</h3>
                         <p className="text-gray-400">
-                          Tamu akan tidur di salah satu kamar atau area umum yang mungkin akan digunakan bersama anda atau orang lain
+                          Tamu akan tidur di salah satu kamar atau area umum
+                          yang mungkin akan digunakan bersama anda atau orang
+                          lain
                         </p>
                       </div>
                       <IoPeopleSharp className="w-10 h-10 flex flex-shrink-0" />
@@ -145,48 +162,152 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
                   </div>
                 </div>
               </Tabs.Panel>
-              <Tabs.Panel value={2}>
+              <Tabs.Panel value={STEPS.LOCATION}>
                 <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
-                  <h2 className="text-2xl font-medium">Di mana lokasi tempat Anda ?</h2>  
-                  <p>Alamat Anda hanya akan diberitahukan kepada tamu setelah anda melakukan reservasi</p>
-                  <div className="w-full aspect-square  bg-gray-100 mt-4 rounded-lg">
-
-                  </div>
+                  <h2 className="text-3xl font-medium">
+                    Di mana lokasi tempat Anda ?
+                  </h2>
+                  <p>
+                    Alamat Anda hanya akan diberitahukan kepada tamu setelah
+                    anda melakukan reservasi
+                  </p>
+                  <div className="w-full aspect-square  bg-gray-100 mt-4 rounded-lg"></div>
                 </div>
               </Tabs.Panel>
-              <Tabs.Panel value={3}>
+              <Tabs.Panel value={STEPS.INFO}>
                 <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
-                  <h2 className="text-2xl font-medium ">Sampaikan informasi dasar mengenai tempat Anda</h2>
-                  <p className="text-gray-400">Anda akan menambahkan detail lainnya nanti, seperti tempat tidur.</p>
+                  <h2 className="text-3xl font-medium ">
+                    Sampaikan informasi dasar mengenai tempat Anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Anda akan menambahkan detail lainnya nanti, seperti tempat
+                    tidur.
+                  </p>
                   <div className="mt-7 divide-y-2 divide-gray-200/60">
                     <div className="flex items-center justify-between py-6">
                       <span className="text-gray-600">Tamu</span>
-                      <Stepper setValue={setInfoDasar} value={infoDasar.tamu} name={"tamu"} />
+                      <Stepper
+                        setValue={setInfoDasar}
+                        value={infoDasar.tamu}
+                        name={"tamu"}
+                      />
                     </div>
                     <div className="flex items-center justify-between py-4">
                       <span className="text-gray-600">Kamar</span>
-                      <Stepper setValue={setInfoDasar} value={infoDasar.kamar} name={"kamar"}/>
+                      <Stepper
+                        setValue={setInfoDasar}
+                        value={infoDasar.kamar}
+                        name={"kamar"}
+                      />
                     </div>
                     <div className="flex items-center justify-between py-4">
                       <span className="text-gray-600">Tempat tidur</span>
-                      <Stepper setValue={setInfoDasar} value={infoDasar.tempatTidur} name={"tempatTidur"}/>
+                      <Stepper
+                        setValue={setInfoDasar}
+                        value={infoDasar.tempatTidur}
+                        name={"tempatTidur"}
+                      />
                     </div>
                     <div className="flex items-center justify-between py-4">
                       <span className="text-gray-600">Kamar Mandi</span>
-                      <Stepper setValue={setInfoDasar} value={infoDasar.kamarMandi} name={"kamarMandi"}/>
+                      <Stepper
+                        setValue={setInfoDasar}
+                        value={infoDasar.kamarMandi}
+                        name={"kamarMandi"}
+                      />
                     </div>
                   </div>
                 </div>
               </Tabs.Panel>
-              <Tabs.Panel value={4}>5</Tabs.Panel>
-              <Tabs.Panel value={5}>6</Tabs.Panel>
+              <Tabs.Panel value={STEPS.FASILITAS}>
+                <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
+                  <h2 className="text-3xl font-medium ">
+                    Beri tahu tamu apa saja yang ditawarkan di tempat anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Anda bisa menambahkan fasilitas lainnya setelah iklan
+                    dipublikasikan.
+                  </p>
+                  <div className="grid mt-8 grid-cols-3 gap-3">
+                    {fasility.map((e) => (
+                      <CheckBox value={e.value} name={e.label}>
+                        <e.icon className="w-8 h-8 flex flex-shrink-0" />
+                        <h3 className="mt-3">{e.label}</h3>
+                      </CheckBox>
+                    ))}
+                  </div>
+                </div>
+              </Tabs.Panel>
+              <Tabs.Panel value={STEPS.IMAGES}>
+                <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
+                  <h2 className="text-3xl font-medium ">
+                    Tambahkan beberapa foto kabin anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Anda membutuhkan 5 foto untuk memulai. Anda bisa menambahkan
+                    foto atau mengubahnya nanti
+                  </p>
+                  <div className="mt-8  gap-3">
+                    <FileInput className="w-full aspect-[7/5]" />
+                  </div>
+                </div>
+              </Tabs.Panel>
+              <Tabs.Panel value={STEPS.JUDUL}>
+                <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
+                  <h2 className="text-3xl font-medium ">
+                    Sekarang, mari beri judul untuk kabin anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Judul ringkas adalah yang terbaik. Anda bisa mengubahnya
+                    kapan saja.
+                  </p>
+                  <div className="mt-8 ">
+                    <TextArea
+                      value={title}
+                      row={5}
+                      setValue={setTitle}
+                      maxLength={32}
+                    />
+                  </div>
+                </div>
+              </Tabs.Panel>
+              <Tabs.Panel value={STEPS.DESCRIPTION}>
+                <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
+                  <h2 className="text-3xl font-medium ">
+                    Tuliskan deskripsi tempat Anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Sampaikan apa yang membuat tempat Anda istimewa
+                  </p>
+                  <div className="mt-8">
+                    <TextArea
+                      value={description}
+                      row={8}
+                      setValue={setDescription}
+                      maxLength={500}
+                    />
+                  </div>
+                </div>
+              </Tabs.Panel>
+              <Tabs.Panel value={STEPS.PRICE}>
+                <div className="w-full h-full max-w-2xl mx-auto flex flex-col justify-center">
+                  <h2 className="text-3xl font-medium ">
+                    Sekarang tetapkan harga Anda
+                  </h2>
+                  <p className="text-gray-400 mt-1">
+                    Tetapkan harga anda untuk tempat anda, anda dapat
+                    mengubahnya kapan saja.
+                  </p>
+                  <div className="mt-8 "></div>
+                </div>
+              </Tabs.Panel>
             </Tabs.Panels>
           </Tabs>
         </div>
         <div className="mt-auto flex-shrink-0">
           <div className="w-full bg-gray-200 h-[.35rem] overflow-hidden">
             <div
-              className="h-full bg-black transition-all duration-700 "
+              className="h-full bg-black transition-all delay-300 duration-700 "
               style={{
                 width: `${(currentStep / STEPS.PRICE) * 100}%`,
               }}
@@ -195,14 +316,15 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
           <div className="w-full px-5 py-3 flex justify-between">
             <Button
               disabled={currentStep == 0}
-              className=" underline"
-              onClick={() => setCurrentStep((e) => e - 1)}
+              className="underline disabled:text-gray-300"
+              onClick={prev}
             >
               Kembali
             </Button>
+
             <Button
-              className="bg-slate-800 hover:bg-black  rounded-lg px-8 py-3 text-white"
-              onClick={() => setCurrentStep((e) => e + 1)}
+              className="bg-slate-800 ml-auto hover:bg-black  rounded-lg px-8 py-3 text-white"
+              onClick={next}
             >
               Berikutnya
             </Button>
@@ -212,8 +334,5 @@ const CreateNewBnbModal = ({ isOpen, onClose }) => {
     </Modal>
   );
 };
-
-
-
 
 export default CreateNewBnbModal;
